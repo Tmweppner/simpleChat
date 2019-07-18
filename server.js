112ddb7068c5420b4d4fe8ad68701ec72373faeb
tmweppner <tmweppne@buffalo.edu>
2019-07-18 14:14:24 -0400

    i think im almost there

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ io.sockets.on('connection', function(socket){
 
     //Disconnect
     socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket),1);
         console.log('Disconnected: %s sockets connected', connections.length);  
     });
@@ -28,4 +30,21 @@ io.sockets.on('connection', function(socket){
     socket.on('send message', function(data){
         io.sockets.emit('new message', {msg: data});
     });
+
+
+    //new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+
+    }
+
+
 });
+
